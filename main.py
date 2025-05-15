118ba4b89335746fe196f673bb625efb7a7e2c24
NonExstnt <64533120+NonExstnt@users.noreply.github.com>
2025-05-15 23:29:48 +1000

    Asteroids Splitting functionality implemented

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def main():
         for asteroid in asteroids:
             for shot in shots:
                 if asteroid.is_colliding(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
             if asteroid.is_colliding(player):
                 sys.exit("Game over!")
